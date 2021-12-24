f023df86ab8fbc0ad6bdb7a620be7f0ab4badfac
takoba <plane25@gmail.com>
2021-12-25 02:34:11 +0900

    :rotating_light: add test file for `<Looseleaf />`

---
+++ src/components/molecules/Looseleaf.test.tsx
@@ -0,0 +1,12 @@
+import {render, screen} from "@testing-library/react"
+import React from 'react'
+import Looseleaf from "components/molecules/Looseleaf"
+
+describe('<Note />', () => {
+  test('should render component', () => {
+    const text = 'Lorem ipsum dolor sit amet'
+    render(<Looseleaf>{text}</Looseleaf>)
+
+    expect(screen.getByText(text)).toBeInTheDocument()
+  })
+})
