ed5c2a6fa033a5c9ac07c656fc1da477b0202db9
takoba <plane25@gmail.com>
2021-12-25 02:40:15 +0900

    :+1: improve `<Looseleaf />` component

--- src/components/molecules/Looseleaf.tsx
+++ src/components/molecules/Looseleaf.tsx
@@ -1,25 +1,24 @@
 import React from "react"
 import styled from "styled-components"
 
-const Looseleaf: React.FC = ({ children }): JSX.Element => {
-  const Container = styled.div`
-    background-image:
-      linear-gradient(90deg, transparent 0%, transparent 50%, #fff 50%, #fff 100%),
-      linear-gradient(180deg, transparent 0%, transparent 97%, #9cc 97%, #9cc 100%);
-    background-size:
-      8px 100%,
-      100% 2.5rem;
-    line-height: 2.5rem;
-  `
-  const Text = styled.p`
-    overflow-wrap: break-word;
-  `
+const Container = styled.div`
+  background-image:
+    linear-gradient(90deg, transparent 0%, transparent 50%, #fff 50%, #fff 100%),
+    linear-gradient(180deg, transparent 0%, transparent 97%, #9cc 97%, #9cc 100%);
+  background-size:
+    8px 100%,
+    100% 2.5rem;
+  line-height: 2.5rem;
+`
 
-  return (
-    <Container>
-      <Text>{children}</Text>
-    </Container>
-  )
-}
+const Text = styled.p`
+  overflow-wrap: break-word;
+`
+
+const Looseleaf: React.FC = ({ children }): JSX.Element => (
+  <Container>
+    <Text>{children}</Text>
+  </Container>
+)
 
 export default Looseleaf
