5609c642aa499fce66d27901320f29e846fcf3f5
takoba <plane25@gmail.com>
2021-12-25 00:10:13 +0900

    :tada: Webpackを利用してreact-appを`yarn webpack serve`で起動できるようにする

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (): JSX.Element => (
+  <>
+    <h1>takoba/react-testing-library-examples</h1>
+    <p>Sandbox with react + jest + @testing-library/react.</p>
+  </>
+)
+
+ReactDOM.render(<App />, document.getElementById('react-root'))
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TsconfigPathsWebpackPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    app: './src/App.tsx',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', 'json'],
+    plugins: [new TsconfigPathsWebpackPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 9003,
+    static: {
+      directory: __dirname,
+    },
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /^\/*/,
+          to: '/index.html',
+        },
+      ],
+    },
+  },
+}
