4ad26e7a25dbd26d35dda6dc81c8bc644fef0e3d
takoba <plane25@gmail.com>
2021-12-25 03:18:05 +0900

    :wrench: add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    'plugin:import/errors',
+    'plugin:import/typescript',
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2022,
+    sourceType: "module"
+  },
+  plugins: [
+    "react",
+    "import",
+    "jest",
+    "@typescript-eslint",
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off',
+    'sort-imports': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off'
+      }
+    },
+    {
+      files: ['**/*.test.*'],
+      env: {
+        'jest/globals': true
+      },
+      extends: ['plugin:jest/recommended', 'plugin:jest/style']
+    }
+  ]
+};
